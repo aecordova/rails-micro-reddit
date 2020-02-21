e904948f15abec4d1b1e8955a8d11bdf3ce6069f
Rey David Cuevas <cuevas.vela@gmail.com>
2020-02-21 16:04:30 -0600

    fixed rubocop lints

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-    validates :text, presence: true
-    belongs_to :user
-    belongs_to :post
+  validates :text, presence: true
+  belongs_to :user
+  belongs_to :post
 end
