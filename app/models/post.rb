dadbb9586430593201186c1e4daf1c0499be2787
Rey David Cuevas <cuevas.vela@gmail.com>
2020-02-21 17:11:19 -0600

    Added length validation to comment and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
-  validates :text, presence: true
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :text, presence: true, length: { maximum: 1000 }
   belongs_to :user
   has_many :comment
 end
